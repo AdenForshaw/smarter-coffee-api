61d82804e4b3f6485e9121da85dea5e32435e89c
Aden Forshaw <aden.forshaw@gmail.com>
2015-12-09 21:53:25 +0000

    Added code comments for clarity

--- smarter-coffee-api.py
+++ smarter-coffee-api.py
@@ -2,20 +2,38 @@
 import sys
 import socket
 import json
+
+#method names to validate
+API_METHOD_BREW = "brew"
+
+#IP address of the smarter coffee machine on your network
 TCP_IP = '192.168.1.61'
 TCP_PORT = 2081
 BUFFER_SIZE = 10
-MESSAGE = "7"
 
+#default method to call
+api_method = API_METHOD_BREW
+
+if api_method == API_METHOD_BREW:
+	message_to_send = "7"
+}
+
+#make connection to machine and send message
+#TODO: add error checking
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
-s.send(MESSAGE)
+s.send(message_to_send)
 data = s.recv(BUFFER_SIZE)
 s.close()
+
+#convert response from machine to unicode
 return_code = unicode(data)
+
+#set default values to ouput
 success=0
 message=""
 
+#find out what the machine response means
 if return_code =="\x03\x00~":
 	success=1
 	message="brewing"
@@ -28,6 +46,7 @@ elif return_code=="\x03\x06~":
 else:
 	message="check machine"
 
+#ouput JSON to whatever called this script
 print json.dumps({'success': success,'message':message,'return_code':repr(data)[1:10]})
 
 quit()
